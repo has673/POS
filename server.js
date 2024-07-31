c252913dbde41ade67e6cd9e8a139ea94c6bbe11
has673 <hassansajjas>
2024-07-31 21:55:47 +0500

    new

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const Catrouter = require('./Routes/Category');
-const Authrouter = require('./Routes/Auth');
-const Adminrouter = require('./Routes/Admin');
-const Employeerouter = require('./Routes/Employee');
-const Itemrouter = require('./Routes/Item');
+const Catrouter = require('./routes/Category');
+const Authrouter = require('./routes/Auth');
+const Adminrouter = require('./routes/Admin');
+const Employeerouter = require('./routes/Employee');
+const Itemrouter = require('./routes/Item');
 const { swaggerUi, swaggerSpec } = require('./swagger');
 
 // Middleware
@@ -14,7 +14,7 @@ app.use(express.json()); // For parsing application/json
 // Swagger documentation route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Routes
+// routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
